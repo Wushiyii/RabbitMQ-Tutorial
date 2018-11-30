21e91c90818c3595ad38133ba3a592d2d7bf27ed
jessyy <wgq287676521@qq.com>
2018-11-30 17:12:06 +0800

    增加`durable`、`prefetchCount`,`basicQos`

--- src/NewTask.java
+++ src/NewTask.java
@@ -1,22 +1,29 @@
 import com.rabbitmq.client.ConnectionFactory;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.MessageProperties;
 
 import java.util.concurrent.TimeoutException;
 
 public class NewTask {
 
-    private final static String QUEUE_NAME = "hello";
+    private final static String QUEUE_NAME = "task_queue";
 
-    public static void main(String[] argv) throws java.io.IOException, TimeoutException {
+    public static void main(String[] argv)
+            throws java.io.IOException, TimeoutException {
+
         ConnectionFactory factory = new ConnectionFactory();
         factory.setHost("localhost");
         Connection connection = factory.newConnection();
         Channel channel = connection.createChannel();
 
-        channel.queueDeclare(QUEUE_NAME, false, false, false, null);
-        String message = getMessage(new String[]{"Fifth message ....."});
-        channel.basicPublish("", QUEUE_NAME, null, message.getBytes());
+        channel.queueDeclare(QUEUE_NAME, true, false, false, null);
+
+        String message = getMessage(argv);
+
+        channel.basicPublish( "", QUEUE_NAME,
+                MessageProperties.PERSISTENT_TEXT_PLAIN,
+                message.getBytes());
         System.out.println(" [x] Sent '" + message + "'");
 
         channel.close();
--- src/Worker.java
+++ src/Worker.java
@@ -1,23 +1,22 @@
 import com.rabbitmq.client.*;
 
 import java.io.IOException;
-import java.util.concurrent.TimeoutException;
 
 public class Worker {
-    private final static String QUEUE_NAME = "hello";
+    private static final String TASK_QUEUE_NAME = "task_queue";
 
-    public static void main(String[] argv)
-            throws IOException, TimeoutException {
-
+    public static void main(String[] argv) throws Exception {
         ConnectionFactory factory = new ConnectionFactory();
         factory.setHost("localhost");
-        Connection connection = factory.newConnection();
-        Channel channel = connection.createChannel();
+        final Connection connection = factory.newConnection();
+        final Channel channel = connection.createChannel();
 
-        channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+        channel.queueDeclare(TASK_QUEUE_NAME, true, false, false, null);
         System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
 
-        Consumer consumer = new DefaultConsumer(channel) {
+        channel.basicQos(1);
+
+        final Consumer consumer = new DefaultConsumer(channel) {
             @Override
             public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException {
                 String message = new String(body, "UTF-8");
@@ -25,20 +24,25 @@ public class Worker {
                 System.out.println(" [x] Received '" + message + "'");
                 try {
                     doWork(message);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
                 } finally {
                     System.out.println(" [x] Done");
+                    channel.basicAck(envelope.getDeliveryTag(), false);
                 }
             }
         };
-        boolean autoAck = true; // acknowledgment is covered below
-        channel.basicConsume(QUEUE_NAME, autoAck, consumer);
+        boolean autoAck = false;
+        channel.basicConsume(TASK_QUEUE_NAME, autoAck, consumer);
     }
 
-    private static void doWork(String task) throws InterruptedException {
-        for (char ch: task.toCharArray()) {
-            if (ch == '.') Thread.sleep(1000);
+    private static void doWork(String task) {
+        for (char ch : task.toCharArray()) {
+            if (ch == '.') {
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException _ignored) {
+                    Thread.currentThread().interrupt();
+                }
+            }
         }
     }
 }
